62f7d96b396b39a2a6d812886dc76bc39e84462a
Margarita <ritaboyko906151@gmail.com>
2023-12-09 21:18:01 +0200

    Task 9 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val BUNDLE_TV_REMOVED_TAG = "BUNDLE_TV_REMOVED_TAG"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.removeButton)
+        val textView = findViewById<TextView>(R.id.titleTextView)
+        val root = findViewById<LinearLayout>(R.id.rootLayout)
+
+        savedInstanceState?.let {
+            val tvHidden = savedInstanceState.getBoolean(BUNDLE_TV_REMOVED_TAG)
+            if (tvHidden) root.removeView(textView)
+        }
+
+        button.setOnClickListener {
+            root.removeView(textView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(BUNDLE_TV_REMOVED_TAG, true)
     }
 }
