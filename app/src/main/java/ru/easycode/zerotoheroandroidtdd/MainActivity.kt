a33ff30241e666752bd2ba9c2fde7c28d6fe9464
Margarita <ritaboyko906151@gmail.com>
2023-12-10 13:29:49 +0200

    Task 11 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,38 +3,21 @@ package ru.easycode.zerotoheroandroidtdd
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val BUNDLE_TV_REMOVED_TAG = "BUNDLE_TV_REMOVED_TAG"
-
-    }
-    private var tvHidden: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.removeButton)
-        val textView = findViewById<TextView>(R.id.titleTextView)
-        val root = findViewById<LinearLayout>(R.id.rootLayout)
+        val button = findViewById<Button>(R.id.incrementButton)
+        val textView = findViewById<TextView>(R.id.countTextView)
 
-        savedInstanceState?.let {
-            tvHidden = savedInstanceState.getBoolean(BUNDLE_TV_REMOVED_TAG)
-            if (tvHidden) root.removeView(textView)
-            button.isEnabled = !tvHidden
-        }
+        if (savedInstanceState == null) textView.text = (0).toString()
 
         button.setOnClickListener {
-            root.removeView(textView)
-            tvHidden = true
-            it.isEnabled = !tvHidden
+            var cur = textView.text.toString().toInt()
+            textView.text = (cur + 2).toString()
         }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(BUNDLE_TV_REMOVED_TAG, tvHidden)
     }
 }
