7add16903a0a8b8970f17842156fcef38b7f3d1d
Margarita <ritaboyko906151@gmail.com>
2023-12-10 13:29:49 +0200

    Task 11 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.incrementButton)
+        val textView = findViewById<TextView>(R.id.countTextView)
+
+        if (savedInstanceState == null) textView.text = (0).toString()
+
+        button.setOnClickListener {
+            var cur = textView.text.toString().toInt()
+            textView.text = (cur + 2).toString()
+        }
     }
 }
