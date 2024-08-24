ad30f416f8044f3ac62d28bf4f69d524be85664d
Margarita <ritaboyko906151@gmail.com>
2024-08-25 01:02:55 +0300

    Task 30 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +21,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    Hello("Hello World!")
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun Hello(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun HelloPreview() {
+    ZeroToHeroAndroidTDDTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Hello("Hello World!")
         }
     }
 }
