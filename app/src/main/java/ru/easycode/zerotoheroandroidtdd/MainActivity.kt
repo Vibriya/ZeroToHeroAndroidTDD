aa645f620f35ed8dd3d390b625f5b79c70fea865
Margarita <ritaboyko906151@gmail.com>
2024-08-25 16:55:05 +0300

    Task 31 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +27,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    ButtonClick()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ButtonClick() {
+    ElevatedButton(
+        onClick = {},
+        modifier = Modifier.wrapContentSize()
+    ) {
+        Text("Click me!")
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 720, showBackground = true)
+@Composable
+fun PreviewView() {
+    ZeroToHeroAndroidTDDTheme {
+        ButtonClick()
     }
 }
