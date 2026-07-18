e070762fb777e93648d3ec9c686c9064d6ed65f0
marrguee <margaritaboykowork@gmail.com>
2026-07-18 16:00:48 +0200

    task 034 completed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -3,9 +3,17 @@ package ru.easycode.zerotoheroandroidtdd
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import ru.easycode.zerotoheroandroidtdd.ui.theme.ZeroToHeroAndroidTDDTheme
 
@@ -18,7 +26,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //todo
+                    var count by rememberSaveable {
+                        mutableStateOf<Count>(Count.Base(value = 0, step = 1))
+                    }
+                    Column {
+                        Text(
+                            text = count.toString()
+                        )
+                        TextButton(
+                            onClick = { count = count.increment() },
+                            content = { Text(text = "increment") }
+                        )
+                    }
                 }
             }
         }
